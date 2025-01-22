e5c66f0012e49ab02dd1727fc0d02c74de1af233
Deeps2023 <deepshikhas2023@gmail.com>
2025-01-23 02:01:42 +0530

    loading dataset from huggingface

--- class1.py
+++ class1.py
@@ -1,2 +1,14 @@
 import pandas as pd
 import scanpy as sc
+
+from datasets import load_dataset
+
+# Load all splits of the dataset
+dataset = load_dataset("dair-ai/emotion")
+
+# Access train, test, or validation split
+train_data = dataset["train"]
+test_data = dataset["test"]
+
+# Display an example from the train split
+print(train_data[0])
