94baa50813f4164b40acc9b9ac5c2b0832ac40a4
Deeps2023 <deepshikhas2023@gmail.com>
2025-01-29 01:59:23 +0530

    explored emotion counts in train dataset

--- class1.py
+++ class1.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import scanpy as sc
+import pandas as pd
+import matplotlib.pyplot as plt
 
 from datasets import load_dataset
 
@@ -12,3 +14,23 @@ test_data = dataset["test"]
 
 # Display an example from the train split
 print(train_data[0])
+
+#eda
+print(dataset)
+
+import matplotlib.pyplot as plt
+
+# Extract labels from the training dataset
+labels = [example['label'] for example in dataset['train']]
+
+# Count label occurrences
+from collections import Counter
+label_counts = Counter(labels)
+
+# Plot the label distribution
+plt.bar(label_counts.keys(), label_counts.values())
+plt.xticks([0,1,2,3,4,5], ['sadness', 'joy','love','anger','fear','surprise'])
+plt.xlabel('Sentiment')
+plt.ylabel('Count')
+plt.title('Label Distribution in Training Set')
+plt.show()
